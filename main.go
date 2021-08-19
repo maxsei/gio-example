774b8c7ddab68dd02b58d7be6ee03cdfa4e2b12f
mschulte <mschulte@eogresources.com>
2021-08-19 16:46:48 -0500

    flex layout and spacers

--- main.go
+++ main.go
@@ -35,11 +35,28 @@ func main() {
 				// frame event that triggered them.
 				gtx := layout.NewContext(&ops, fe)
 
-				// Style the button according to the theme to get a styled button back.
-				btn := material.Button(theme, &startButton, "Start")
+				// Define flex layout with the following options.
+				layout.Flex{
+					Axis:    layout.Vertical,
+					Spacing: layout.SpaceStart,
+				}.Layout(gtx,
+					// Define the children of the flex layout which in this case is just
+					// button widget. Widget's are just functions of a graphical context
+					// that return their dimensions.
+					layout.Rigid(
+						func(gtx layout.Context) layout.Dimensions {
+							// Style the button according to the theme to get a styled button back.
+							btn := material.Button(theme, &startButton, "Start")
 
-				// Add operations to the graphical context to draw the button.
-				btn.Layout(gtx)
+							// Add operations to the graphical context to draw the button.
+							return btn.Layout(gtx)
+						},
+					),
+					// Add an empty space the bottom of the screen.
+					layout.Rigid(
+						layout.Spacer{Height: unit.Dp(25)}.Layout,
+					),
+				)
 
 				// Add the list of operations to the frame event.
 				fe.Frame(gtx.Ops)
